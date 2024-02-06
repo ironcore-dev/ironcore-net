e15c26db452493acfbf073e347fb26835b9ebb7d
Andreas Fritzler <andreas.fritzler@sap.com>
2024-02-06 16:13:29 +0100

    Fix nil pointer for internal LB type handling in api server (#230)

--- internal/controllers/loadbalancer_controller_test.go
+++ internal/controllers/loadbalancer_controller_test.go
@@ -70,4 +70,58 @@ var _ = Describe("LoadBalancerController", func() {
 			},
 		}))
 	})
+
+	It("should reconcile an internal load balancer", func(ctx SpecContext) {
+		By("creating an internal load balancer")
+		loadBalancer := &v1alpha1.LoadBalancer{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace:    ns.Name,
+				GenerateName: "load-balancer-",
+			},
+			Spec: v1alpha1.LoadBalancerSpec{
+				Type:       v1alpha1.LoadBalancerTypeInternal,
+				NetworkRef: corev1.LocalObjectReference{Name: network.Name},
+				IPs: []v1alpha1.LoadBalancerIP{
+					{
+						Name:     "ip-1",
+						IPFamily: corev1.IPv4Protocol,
+					},
+				},
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{"foo": "bar"},
+				},
+				Template: v1alpha1.InstanceTemplate{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{"foo": "bar"},
+					},
+				},
+			},
+		}
+		Expect(k8sClient.Create(ctx, loadBalancer)).To(Succeed())
+		ips := v1alpha1.GetLoadBalancerIPs(loadBalancer)
+
+		By("waiting for the internal load balancer to create a daemon set")
+		daemonSet := &v1alpha1.DaemonSet{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: ns.Name,
+				Name:      v1alpha1.LoadBalancerDaemonSetName(loadBalancer.Name),
+			},
+		}
+		Eventually(Object(daemonSet)).Should(HaveField("Spec", v1alpha1.DaemonSetSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"foo": "bar"},
+			},
+			Template: v1alpha1.InstanceTemplate{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"foo": "bar"},
+				},
+				Spec: v1alpha1.InstanceSpec{
+					Type:             v1alpha1.InstanceTypeLoadBalancer,
+					LoadBalancerType: v1alpha1.LoadBalancerTypeInternal,
+					NetworkRef:       corev1.LocalObjectReference{Name: network.Name},
+					IPs:              ips,
+				},
+			},
+		}))
+	})
 })
