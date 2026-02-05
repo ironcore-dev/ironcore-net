4909e60e925ff65ee52d9da22aba785dd2250924
Daniel Gonzalez <daniel@gonzalez-nothnagel.de>
2026-02-05 14:07:35 +0100

    Run `make generate`

--- client-go/applyconfigurations/core/v1alpha1/networkid.go
+++ client-go/applyconfigurations/core/v1alpha1/networkid.go
@@ -66,6 +66,7 @@ func extractNetworkID(networkID *corev1alpha1.NetworkID, fieldManager string, su
 	b.WithAPIVersion("core.apinet.ironcore.dev/v1alpha1")
 	return b, nil
 }
+func (b NetworkIDApplyConfiguration) IsApplyConfiguration() {}
 
 // WithKind sets the Kind field in the declarative configuration to the given value
 // and returns the receiver, so that objects can be built by chaining "With" function invocations.
@@ -233,8 +234,24 @@ func (b *NetworkIDApplyConfiguration) WithSpec(value *NetworkIDSpecApplyConfigur
 	return b
 }
 
+// GetKind retrieves the value of the Kind field in the declarative configuration.
+func (b *NetworkIDApplyConfiguration) GetKind() *string {
+	return b.TypeMetaApplyConfiguration.Kind
+}
+
+// GetAPIVersion retrieves the value of the APIVersion field in the declarative configuration.
+func (b *NetworkIDApplyConfiguration) GetAPIVersion() *string {
+	return b.TypeMetaApplyConfiguration.APIVersion
+}
+
 // GetName retrieves the value of the Name field in the declarative configuration.
 func (b *NetworkIDApplyConfiguration) GetName() *string {
 	b.ensureObjectMetaApplyConfigurationExists()
 	return b.ObjectMetaApplyConfiguration.Name
+}
+
+// GetNamespace retrieves the value of the Namespace field in the declarative configuration.
+func (b *NetworkIDApplyConfiguration) GetNamespace() *string {
+	b.ensureObjectMetaApplyConfigurationExists()
+	return b.ObjectMetaApplyConfiguration.Namespace
 }
