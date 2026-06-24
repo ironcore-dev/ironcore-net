5a86d8b1cd917883708e006d86911e07d4bc7a79
Andreas Fritzler <andreas.fritzler@sap.com>
2026-06-24 15:45:03 +0200

    Fix NATGateway watch setup in apinetlet (#507)
    Signed-off-by: Andreas Fritzler <andreas.fritzler@sap.com>

--- apinetlet/controllers/natgateway_controller_test.go
+++ apinetlet/controllers/natgateway_controller_test.go
@@ -11,6 +11,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	. "sigs.k8s.io/controller-runtime/pkg/envtest/komega"
 )
@@ -57,6 +58,53 @@ var _ = Describe("NATGatewayController", func() {
 				StemFrom(NATGatewayOrigin, natGateway),
 				BeControlledBy(apiNetNATGateway),
 			),
+		)
+	})
+
+	It("should reconcile natgateway when network is created after natgateway", func(ctx SpecContext) {
+		By("creating a nat gateway whose network does not yet exist")
+		natGateway := &networkingv1alpha1.NATGateway{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace:    ns.Name,
+				GenerateName: "nat-gateway-",
+			},
+			Spec: networkingv1alpha1.NATGatewaySpec{
+				Type:       networkingv1alpha1.NATGatewayTypePublic,
+				IPFamily:   corev1.IPv4Protocol,
+				NetworkRef: corev1.LocalObjectReference{Name: "foo"},
+			},
+		}
+		Expect(k8sClient.Create(ctx, natGateway)).To(Succeed())
+
+		By("asserting no APINet NAT gateway is created while the network is missing")
+		apiNetNATGateway := &v1alpha1.NATGateway{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: apiNetNs.Name,
+				Name:      string(natGateway.UID),
+			},
+		}
+		Consistently(Get(apiNetNATGateway)).Should(Satisfy(apierrors.IsNotFound))
+
+		By("creating the referenced network")
+		network := &networkingv1alpha1.Network{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: ns.Name,
+				Name:      "foo",
+			},
+		}
+		Expect(k8sClient.Create(ctx, network)).To(Succeed())
+
+		By("waiting for the APINet NAT gateway to appear once the network exists")
+		Eventually(Object(apiNetNATGateway)).Should(StemFrom(NATGatewayOrigin, natGateway))
+
+		By("simulating the autoscaler by requesting one public IP on the APINet NAT gateway")
+		Eventually(Update(apiNetNATGateway, func() {
+			apiNetNATGateway.Spec.IPs = []v1alpha1.NATGatewayIP{{Name: "ip-1"}}
+		})).Should(Succeed())
+
+		By("waiting for the NAT gateway status IPs to be populated")
+		Eventually(Object(natGateway)).Should(
+			HaveField("Status.IPs", Not(BeEmpty())),
 		)
 	})
 })
