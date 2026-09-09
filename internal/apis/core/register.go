18d25f2ebcfa2f5cd5ee7d4e7ed1a2d8a1e7360c
Felix Riegger <felix.riegger@sap.com>
2026-09-09 11:16:23 +0200

    Remove year from license headers (#542)
    Signed-off-by: Felix Riegger <felix.riegger@sap.com>

--- internal/apis/core/register.go
+++ internal/apis/core/register.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and IronCore contributors
 // SPDX-License-Identifier: Apache-2.0
 
 // Package core contains API Schema definitions for the apinet core API group
